307fc8e46fb485caa13a768bed7f7c4a3dcecb97
Nicholas Sim <nicholassimws@gmail.com>
2016-04-12 23:09:20 +0100

    templatetags: check input for price formatter
    see issue #4

--- templatetags/mealy_extras.py
+++ templatetags/mealy_extras.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: latin-1 -*-
+from numbers import Number
 from django import template
 from django.utils.html import format_html
 
@@ -7,6 +8,9 @@ register = template.Library()
 
 @register.filter
 def price(pr, p2 = False):
+    if not isinstance(pr, Number):
+        raise TypeError("Price must be a number")
+
     if p2 is False:
         if pr < 0.5:
             return "nothing"
@@ -14,6 +18,10 @@ def price(pr, p2 = False):
         if pr < 100:
             return "%dp" % pr
         return "£%.2f" % (pr/100)
+
+    if not isinstance(p2, Number):
+        raise TypeError("Price must be a number")
+
     if pr < 0.5 and p2 < 0.5:
         return "nothing"
     pr, p2 = round(pr, 0), round(p2, 0)
@@ -26,4 +34,3 @@ def dish_pretty(dish):
     #   Assume that first word is the cooking style
     dish = dish.__str__().partition(" ")
     return format_html("{} <em>{}</em>", dish[0], dish[2])
-    #return "%s <em>%s</em>" % (dish[0], dish[2])
