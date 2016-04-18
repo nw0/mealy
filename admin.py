d6247d56cf557bfec31cd2ba8e9450329e66dac7
Nicholas Sim <nicholassimws@gmail.com>
2016-04-18 22:42:53 +0100

    admin: meal: add filters and columns

--- admin.py
+++ admin.py
@@ -5,8 +5,12 @@ from .models import Resource_Inst, Resource_Ticket, Resource_Type, Meal, Dish
 admin.site.register(Resource_Type)
 admin.site.register(Resource_Inst)
 admin.site.register(Resource_Ticket)
-admin.site.register(Meal)
 admin.site.register(Dish)
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display    = ('__str__', 'meal_owner', 'cons_time', 'meal_type')
+    list_filter     = ('meal_owner', 'meal_type')
 
 def other_checks(user):
     #   Use this function for checks which the user must pass to use Mealy
