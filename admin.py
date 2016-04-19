c2b36882e1e7a6cc2c4e76700e4510b951c190a7
Nicholas Sim <nicholassimws@gmail.com>
2016-04-19 17:27:30 +0100

    admin: dish: add filter

--- admin.py
+++ admin.py
@@ -5,12 +5,15 @@ from .models import Resource_Inst, Resource_Ticket, Resource_Type, Meal, Dish
 admin.site.register(Resource_Type)
 admin.site.register(Resource_Inst)
 admin.site.register(Resource_Ticket)
-admin.site.register(Dish)
 
 @admin.register(Meal)
 class MealAdmin(admin.ModelAdmin):
     list_display    = ('__str__', 'meal_owner', 'cons_time', 'meal_type')
     list_filter     = ('meal_owner', 'meal_type')
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_filter     = ('par_meal__meal_owner', 'cooking_style')
 
 def other_checks(user):
     #   Use this function for checks which the user must pass to use Mealy
