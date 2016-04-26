b609ac331ba4ca125efd0683e61ce0a14006cd1a
Nicholas Sim <nicholassimws@gmail.com>
2016-04-26 22:34:35 +0100

    ttags: price: display pence to 2sf

--- templatetags/mealy_extras.py
+++ templatetags/mealy_extras.py
@@ -12,21 +12,15 @@ def price(pr, p2 = False):
         raise TypeError("Price must be a number")
 
     if p2 is False:
-        if pr < 0.5:
-            return "nothing"
-        pr = round(pr, 0)
         if pr < 100:
-            return "%dp" % pr
+            return "%.2gp" % pr
         return "£%.2f" % (pr/100)
 
     if not isinstance(p2, Number):
         raise TypeError("Price must be a number")
 
-    if pr < 0.5 and p2 < 0.5:
-        return "nothing"
-    pr, p2 = round(pr, 0), round(p2, 0)
     if pr < 100 and p2 < 100:
-        return "%dp + %dp" % (pr, p2)
+        return "%.2gp + %.2gp" % (pr, p2)
     return "£%.2f + £%.2f" % (pr/100, p2/100)
 
 @register.filter
